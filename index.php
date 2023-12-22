f0f82a93d919f06440ee875c14325a44cf4d8365
zhullyb <zhullyb@outlook.com>
2023-12-22 14:55:10 +0800

    take framework

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+switch ($method | $uri) {
+    case ($method == 'POST' && $uri == '/api/v1/user/register'):
+        require __DIR__ . '/api/v1/user/register.php';
+        break;
+    case ($method == 'POST' && $uri == '/api/v1/user/login'):
+        require __DIR__ . '/api/v1/user/login.php';
+        break;
+    case ($method == 'GET' && $uri == '/api/v1/user/getUsers'):
+        require __DIR__ . '/api/v1/user/getUsers.php';
+        break;
+    case ($method == 'GET' && $uri == '/api/v1/user/deleteUser'):
+        require __DIR__ . '/api/v1/user/deleteUser.php';
+        break;
+    case ($method == 'GET' && $uri == '/api/v1/movie/search'):
+        require __DIR__ . '/api/v1/movie/search.php';
+        break;
+    case ($method == 'GET' && $uri == '/api/v1/movie/display'):
+        require __DIR__ . '/api/v1/movie/display.php';
+        break;
+    case ($method == 'GET' && $uri == '/api/v1/movie/getInfo'):
+        require __DIR__ . '/api/v1/movie/getInfo.php';
+        break;
+    case ($method == 'POST' && $uri == '/api/v1/movie/update'):
+        require __DIR__ . '/api/v1/movie/update.php';
+        break;
+    case ($method == 'POST' && $uri == '/api/v1/movie/del'):
+        require __DIR__ . '/api/v1/movie/del.php';
+        break;        
+    case ($method == 'POST' && $uri == '/api/v1/movie/add'):
+        require __DIR__ . '/api/v1/movie/add.php';
+        break;
+    case ($method == 'GET' && $uri == '/api/v1/comment/getList'):
+        require __DIR__ . '/api/v1/comment/getList.php';
+        break;
+    case ($method == 'POST' && $uri == '/api/v1/comment/add'):
+        require __DIR__ . '/api/v1/comment/add.php';
+        break;
+    case ($method == 'DELETE' && $uri == '/api/v1/comment/del'):
+        require __DIR__ . '/api/v1/comment/del.php';
+        break;
+    case ($method == 'PUT' && $uri == '/api/v1/comment/edit'):
+        require __DIR__ . '/api/v1/comment/edit.php';
+        break;
+    default:
+        echo 'API Running';
+        break;
+}
+?>
