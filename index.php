7fd0836f7f4ada28c2d7457aeba324567d37c9c2
zhullyb <zhullyb@outlook.com>
2023-12-23 16:53:10 +0800

    index.php: fix CORS

--- index.php
+++ index.php
@@ -7,6 +7,11 @@ session_start();
 include_once __DIR__ . '/config/db.php';
 require_once __DIR__ . '/utils/isAdmin.php';
 
+header('Access-Control-Allow-Origin: http://localhost:5173');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+
 switch ($method | $uri) {
     case ($method == 'POST' && $uri == '/api/v1/user/register'):
         require __DIR__ . '/api/v1/user/register.php';
