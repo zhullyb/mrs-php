2d44db559630f7b51f76f14d745a46c86ef4a7c2
zhullyb <zhullyb@outlook.com>
2023-12-27 23:41:52 +0800

    Set cookie_lifetime

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 ini_set('session.gc_maxlifetime', 60*60*24);
+ini_set('session.cookie_lifetime', 60*60*24);
 session_start();
 
 include_once __DIR__ . '/config/db.php';
